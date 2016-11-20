53e8fb93a808fdfaaeb83339926e6865a0cfb08b
Johan Sanchez <frst008@gmail.com>
2016-11-19 20:08:18 -0600

    Changes in login

--- app/src/main/java/com/soccer/whosin/utils/Constants.java
+++ app/src/main/java/com/soccer/whosin/utils/Constants.java
@@ -6,7 +6,7 @@ package com.soccer.whosin.utils;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://192.168.1.106:8000/api/";
+    public static final String BASE_URL = "http://192.168.1.146:8000/api/";
 
     // SharedPreferences values
     public static final String STORAGE_NAME = "Who is in?";
