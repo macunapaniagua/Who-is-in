091e3f39c91db762ab18a091b8981d5773c451a4
Johan Sanchez <frst008@gmail.com>
2016-11-20 17:05:23 -0600

    Changes in soccergame migration

--- web_api/database/migrations/2016_11_12_224021_create_soccer_games_table.php
+++ web_api/database/migrations/2016_11_12_224021_create_soccer_games_table.php
@@ -19,7 +19,7 @@ class CreateSoccerGamesTable extends Migration
           $table->integer('soccer_field_id');
           $table->foreign('soccer_field_id')->references('id')->on('soccer_fields')->onDelete('cascade');
           $table->integer('players_limit');
-          $table->date('hour');
+          $table->string('hour');
           $table->date('date');
           $table->timestamps();
       });
