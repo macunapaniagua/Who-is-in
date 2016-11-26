9cb44380e33dcf494167dfa5c7800bac9260392b
Johan Sanchez <frst008@gmail.com>
2016-11-26 10:31:02 -0600

    Group messages

--- web_api/app/Lib/UserAuth.php
+++ web_api/app/Lib/UserAuth.php
@@ -5,13 +5,13 @@ class UserAuth
 {
   public static function getUserAuth($request) {
       $facebook_id = $request->header('facebook_id');
-      $user_auth = User::where('facebook_id', $facebook_id)->get()->first();
-      if (!isset($facebook_id)) {
-        return response(trans('api_messages.error_facebook_id'));
+      if (isset($facebook_id)) {
+        $user_auth = User::where('facebook_id', $facebook_id)->get()->first();
+        if (!isset($user_auth)) {
+          return response(trans('api_messages.error_player'));
+        }
+        return User::find($user_auth->id);
       }
-      if (isset($facebook_id) && !isset()$user_auth)) {
-        return response(trans('api_messages.error_player'));
-      }
-      return User::find($user_auth->id);
+      return response(trans('api_messages.error_facebook_id'));
   }
 }
